2f1ad16d69c3c8a87093f39091058f3bc9fc3501
James Kelsey <jamesmskelsey@gmail.com>
2019-09-15 18:36:57 -0700

    initial commit
    "finished", but initial commit

---
+++ ComplimentsAsService/Controllers/ComplimentsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ComplimentsAsService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComplimentsAsService.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class ComplimentsController : Controller
+    {
+        
+        [HttpGet("/hello")]
+        public IActionResult Hello()
+        {
+            
+            return CreateResponse(
+                $"Hello, world",
+                "-Everyone"
+                );
+        }
+
+        [HttpGet("hello/{name}.{format?}")]
+        public IActionResult HelloName(string name, string format)
+        {
+            return CreateResponse(
+                $"Hello, {name}",
+                "",
+                format
+                );
+        }
+
+        [HttpGet("cute/{name}/{from}.{format?}")]
+        public IActionResult CuteNameFrom(string name, string from, string format = "")
+        {
+            return CreateResponse(
+                $"Heeey, {name}, you're cute!",
+                $"-{from}",
+                format
+                );
+            
+        }
+
+        private IActionResult CreateResponse(string message, string subtitle = "", string format = "")
+        {
+            ComplimentModel compliment = new ComplimentModel
+            {
+                Message = message,
+                Subtitle = subtitle,
+            };
+            if (format == "json")
+            {
+                return Ok(compliment);
+            }
+            else
+            {
+                return View("index", compliment);
+                //return Content($"{message} {subtitle}");
+            }
+        }
+    }
+}
---
+++ ComplimentsAsService/Models/ComplimentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComplimentsAsService.Models
+{
+    public class ComplimentModel
+    {
+        public string Message { get; set; }
+        public string Subtitle { get; set; }
+    }
+}
